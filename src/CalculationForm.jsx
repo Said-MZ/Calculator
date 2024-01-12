c0c3130e8f5f9c0a9fb613d8fdaf04971bf988a0
Said zain <saidzain897@gmail.com>
2024-01-12 03:06:56 +0300

    fix import

--- src/CalculationForm.jsx
+++ src/CalculationForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import buttons from "./data";
-import Button from "./button";
+import Button from "./Button";
 const CalculationForm = () => {
   return (
     <div className="grid grid-cols-4 gap-4 w-[90vw] h-[70vh] sm:w-[26rem] sm:h-[32rem] mx-auto">
