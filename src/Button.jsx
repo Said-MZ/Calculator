c9bf171bfbc3836f3ccc4464ee9161d9cddd5e80
Said zain <saidzain897@gmail.com>
2024-01-12 03:27:31 +0300

    fix hover effect

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,13 +3,17 @@ import { useGlobalContext } from "./context";
 
 const Button = ({ label, color }) => {
   const { handleClick, clear } = useGlobalContext();
-  const btnStyles = `${label === 0 ? "col-span-2" : ""} ${
-    color === "grey-light" && "text-black"
+  const btnStyles = `${label === 0 && "col-span-2"} ${
+    color === "bg-greyLight" && "text-black"
   } ${color} rounded-full ${
-    color === "grey-light" ? "text-3xl sm:text-4xl" : "text-3xl sm:text-5xl "
-  } font-medium hover:filter hover:brightness-${
-    color === "grey-dark" ? "150" : "125"
-  } transition-all hover:saturate-50 ${label !== "0' && aspect-square"}  ${
+    color === "bg-greyLight" ? "text-3xl sm:text-4xl" : "text-3xl sm:text-5xl "
+  } font-medium hover:filter focus:filter ${
+    color === "bg-greyDark" ? " hover:brightness-150" : "hover:brightness-125"
+  } ${
+    color === "bg-greyDark" ? "focus:brightness-150" : "focus:brightness-125"
+  }  transition-all focus:saturate-50 hover:saturate-50 ${
+    label !== "0' && aspect-square"
+  }  ${
     label !== 0 ? "w-[20vw] h-[10vh]" : "h-[10vh]"
   } flex justify-center items-center sm:w-auto sm:h-auto`;
 
