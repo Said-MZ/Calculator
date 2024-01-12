90758101beb22bc60c929dc3703b51bbb4f97317
Said zain <saidzain897@gmail.com>
2024-01-12 03:16:52 +0300

    fix style for small screens

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Result from "./Result";
 
 function App() {
   return (
-    <main className="w-screen h-screen overflow-hidden text-white bg-black flex flex-col align-center justify-end sm:justify-center pt-10">
+    <main className="py-16 sm:py-0 w-screen h-screen overflow-hidden text-white bg-black flex flex-col align-center justify-end sm:justify-center pt-10">
       <Result />
       <CalculationForm />
     </main>
